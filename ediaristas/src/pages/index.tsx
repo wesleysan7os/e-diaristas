ad9ab8c2ea918459a86861874e6ef9c67d099708
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-10-09 13:41:18 -0300

    finishing UserInformation component

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,17 +1,27 @@
-import type { NextPage } from 'next';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
       <PageTitle
-        title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+
+      <UserInformation
+        name={"Wesley Santos"}
+        picture={"https://github.com/wesleysan7os.png"}
+        rating={4}
+        description={"Sergipe"}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- ediaristas/src/ui/components/surfaces/Header/Header.style.tsx
+++ ediaristas/src/ui/components/surfaces/Header/Header.style.tsx
@@ -6,12 +6,12 @@ export const HeaderAppBar = styled(AppBar)`
   box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.05);
 
   ${({ theme }) => theme.breakpoints.up("md")} {
-    MuiToobar-root {
+    .MuiToolbar-root {
       height: 100px;
     }
   }
   ${({ theme }) => theme.breakpoints.down("md")} {
-    MuiToobar-root {
+    .MuiToolbar-root {
       display: flex;
       justify-content: center;
     }
